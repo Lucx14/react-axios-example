969c2d4f69a8c370d7ca8729a924a53e77de44b8
Lucien Najev <luciennajev@hotmail.com>
2019-09-07 18:17:14 +0100

    routing: set up initial routing home page and new post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import styles from './Blog.module.css';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
@@ -18,12 +18,19 @@ class Blog extends Component {
         <header className={styles.Blog}>
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to={{
+              <li><NavLink
+                to="/"
+                exact
+                activeClassName="my-active"
+                activeStyle={{
+                  color: '#fa923f',
+                  textDecoration: 'underline'
+                }}>Home</NavLink></li>
+              <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
                 search: '?quick-submit=true'
-              }}>New Post</Link></li>
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>

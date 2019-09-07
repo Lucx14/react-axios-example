28a109d8bb23352b0ecb3b5639607fd10f019472
Lucien Najev <luciennajev@hotmail.com>
2019-09-07 16:54:45 +0100

    new: install react router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import styles from './Blog.module.css';
 
 class Blog extends Component {
